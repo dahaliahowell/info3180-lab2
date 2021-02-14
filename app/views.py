8238b9aa5f84ae7d09f620f1ec3925aa0c1d3ba6
dahaliahowell <dahaliahowell.dh@gmail.com>
2021-02-14 00:52:34 -0500

    Complete profile page

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -23,6 +23,11 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile/')
+def profile():
+    """Render website's profile page."""
+    return render_template('profile.html', date=format_date_joined(datetime.date(2020, 10, 24)))
 
 
 ###
@@ -53,6 +58,9 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
+def format_date_joined(date):
+    return date.strftime("%B, %Y")
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="8080")
+    app.run(debug=True, host="0.0.0.0", port=8080)
